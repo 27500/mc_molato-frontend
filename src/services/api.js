36977c8d757b640fd14ad84e943e829b9a86802f
27500 <blessingmingenge@gmail.com>
2026-08-10 15:57:08 +0100

    fix mis à jour de l'api vers render

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://mc-molato-backend-sigma.vercel.app/api';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://mc-molato-backend.onrender.com/api';
